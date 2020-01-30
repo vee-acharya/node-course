1dda6c82a7945fa4882027177f0d3af9f461612f
Vishal <vishal.acharya@soprasteria.com>
2020-01-30 15:56:29 +0530

    Finalizing Weather app

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,27 @@
-console.log('Client side JS file is loaded.');
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  messageOne.textContent = 'Fetching forecast for provided location...';
+  messageTwo.textContent = '';
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+    response.json().then(data => {
+      if (data.errorMessage) {
+        messageOne.textContent = data.errorMessage;
+        return console.log(data.errorMessage);
+      }
+
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forecast;
+      console.log(data.forecast);
+      console.log(data.location);
+    });
+  });
+});
